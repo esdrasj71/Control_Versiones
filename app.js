3014c49fdb6fee2e6082d0111c3327488c885866
Andres Rivera <riveraandres1997@gmail.com>
2020-09-19 18:13:59 -0600

    commit nuevo

---
+++ app.js
@@ -0,0 +1,18 @@
+const express= require('express');
+const mysql= require('mysql');
+
+const bodyParser=require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+
+const app=express();
+
+app.use(bodyParser().json());
+
+//mysql
+const connection=  mysql.createConnection({
+    host     : 'localhost',
+    user     : 'localhost',
+    password : '',
+    database : 'GG'
+});
