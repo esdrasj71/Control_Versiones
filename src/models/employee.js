c67cf15757a0f7931efbbcc4f62aa17caef00b17
Jul10CC <julio1cayax@outlook.es>
2020-09-25 20:42:59 -0600

    Product Model 20:42

---
+++ src/models/employee.js
@@ -0,0 +1,115 @@
+const sql = require("../conexion.js");
+
+// constructor
+const Employee = function(employee) {
+  this.DPI = employee.DPI;
+  this.Names = employee.Names;
+  this.Last_names = employee.Last_names;
+  this.Phone_number = employee.Phone_number;
+  this.Cellphone_number = employee.Cellphone_number;
+  this.Email = employee.Email;
+  this.Gender = employee.Gender;
+  this.Hire_date = employee.Hire_date;
+  this.Employee_Position_Id = employee.Employee_Position_Id;
+};
+
+//CRUD
+Employee.create = (newEmployee, result) => {
+  sql.query("INSERT INTO Employee SET ?", newEmployee, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+    console.log("Empleado creado: ", { id: res.insertId, ...newEmployee });
+    result(null, { id: res.insertId, ...newEmployee });
+  });
+};
+
+Employee.findById = (employeeId, result) => {
+  sql.query(`SELECT * FROM Employee WHERE Employee_Id = ${employeeId}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("Empleado encontrado: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+
+    // not found Employee with the id
+    result({ kind: "no encontrado" }, null);
+  });
+};
+
+Employee.getAll = result => {
+  sql.query("SELECT * FROM Employee", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("Empleados: ", res);
+    result(null, res);
+  });
+};
+
+Employee.updateById = (id, employee, result) => {
+  sql.query(
+    "UPDATE Employee SET DPI = ?, Names = ?, Last_names = ?, Phone_number = ?, Cellphone_number = ?, Email = ?, Gender = ?, Hire_date = ?, Employee_Position_Id = ? WHERE Employee_Id = ?",
+    [employee.DPI, employee.Names, employee.Last_names, employee.Phone_number, employee.Cellphone_number, employee.Email, employee.Gender, employee.Hire_date, employee.Employee_Position_Id, id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        // not found Employee with the id
+        result({ kind: "no encontrado" }, null);
+        return;
+      }
+      console.log("Empleado actualizado: ", { id: id, ...employee });
+      result(null, { id: id, ...employee });
+    }
+  );
+};
+
+Employee.remove = (id, result) => {
+  sql.query("DELETE FROM Employee WHERE Employee_Id = ?", id, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    if (res.affectedRows == 0) {
+      // not found Employee with the id
+      result({ kind: "no encontrado" }, null);
+      return;
+    }
+
+    console.log("Empleado eliminado con ID: ", id);
+    result(null, res);
+  });
+};
+
+Employee.removeAll = result => {
+  sql.query("DELETE FROM Employee", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log(`Empleados ${res.affectedRows} eliminados`);
+    result(null, res);
+  });
+};
+
+module.exports = Employee;
