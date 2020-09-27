6d732b562e47c9603695dcc3149ac4cff862ae79
Jul10CC <julio1cayax@outlook.es>
2020-09-26 22:25:25 -0600

    Se cambio a product_category routes

--- src/routes/product_category.routes.js
+++ src/routes/product_category.routes.js
@@ -2,10 +2,10 @@ module.exports = app => {
     const product_category = require("../controllers/product_category.controller.js");
 
       //Product categories
-      app.post("/product_categories", product_category.create);
-      app.get("/product_categories", product_category.findAll);
-      app.get("/product_categories/:product_categoryId", product_category.findOne);
-      app.put("/product_categories/:product_categoryId", product_category.update);
-      app.delete("/product_categories/:product_categoryId", product_category.delete);
+      app.post("/product_category", product_category.create);
+      app.get("/product_category", product_category.findAll);
+      app.get("/product_category/:product_categoryId", product_category.findOne);
+      app.put("/product_category/:product_categoryId", product_category.update);
+      app.delete("/product_category/:product_categoryId", product_category.delete);
       
 };
