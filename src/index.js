7ddc2a3f71a8c8ab4965da777f9e1f065047ffd1
Andres Rivera <riveraandres1997@gmail.com>
2020-10-04 18:52:52 -0600

    PROCEDIMIENTO ALMACENADO EN COMPRAS

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ require("./routes/purchase_detail.routes")(app);
 require("./routes/customers.routes")(app);
 require("./routes/employee.routes")(app);
 require("./routes/employee_position.routes")(app);
-
+require("./routes/procedure_purchase.routes")(app);
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
 });
