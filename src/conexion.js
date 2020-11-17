ee9a48cfcde8bab06621ccf3ca4db0a67a9273e3
Andres Rivera <riveraandres1997@gmail.com>
2020-11-16 18:01:35 -0600

    miconexion cluster

--- src/conexion.js
+++ src/conexion.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 console.log('Conectando a la base de datos');
 
 var conection = mysql.createConnection({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: '',
+    host: '204.2.63.54',
+    port: '15388',
+    user: 'andres',
+    password: '123andres',
     database: 'proyectogg'
 })
 
