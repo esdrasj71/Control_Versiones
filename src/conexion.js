0b50cf8a79c7aba5799cbc365c4469db4507d37d
chechitajr20 <69736248+chechitajr20@users.noreply.github.com>
2020-11-18 01:42:28 -0600

    CONEXION

--- src/conexion.js
+++ src/conexion.js
@@ -2,12 +2,13 @@ const mysql = require('mysql');
 console.log('Conectando a la base de datos');
 
 var conection = mysql.createConnection({
-    host: '204.2.63.54',
-    port: '15388',
-    user: 'andres',
-    password: '123andres',
+    host: '172.107.32.129',
+    port: '15390',
+    user: 'kevin',
+    password: '49853095',
     database: 'proyectogg'
 })
+
 
 conection.connect(function(err){
     if(err){
