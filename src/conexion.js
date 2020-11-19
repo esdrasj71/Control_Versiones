ca37433cf95e163c61a4deb8ac16a951c9d4a53b
Andres Rivera <riveraandres1997@gmail.com>
2020-11-18 21:14:32 -0600

    asdasd

--- src/conexion.js
+++ src/conexion.js
@@ -2,22 +2,14 @@ const mysql = require('mysql');
 console.log('Conectando a la base de datos');
 
 var conection = mysql.createConnection({
-
-    host: '172.107.32.129',
-    port: '15390',
-    user: 'kevin',
-    password: '49853095',
+    host: '204.2.63.54',
+    port: '15388',
+    user: 'andres',
+    password: '123andres',
     database: 'proyectogg'
 })
 
 
-conection.connect(function(err){
-    if(err){
-        console.log('Error de conexion'+ err.stack);
-    }else{
-        console.log('conexion exitosa');
-    }
-})
     
 module.exports = conection;
 // el cotejamiento: utf8_spanish2_ci
