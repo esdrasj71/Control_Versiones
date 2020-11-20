8b9d4679ef5761b42f43b150709c49d91050041a
chechitajr20 <69736248+chechitajr20@users.noreply.github.com>
2020-11-19 21:48:54 -0600

    correcciones

--- src/conexion.js
+++ src/conexion.js
@@ -2,14 +2,14 @@ const mysql = require('mysql');
 console.log('Conectando a la base de datos');
 
 var conection = mysql.createConnection({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: '',
+    host: 'mysql-15390-0.cloudclusters.net',
+    port: '15390',
+    user: 'kevin',
+    password: '49853095',
     database: 'proyectogg'
 })
 
 
-    
 module.exports = conection;
+
 // el cotejamiento: utf8_spanish2_ci
