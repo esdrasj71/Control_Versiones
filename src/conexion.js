becad8f336ae137cf5c9991052dc70a3cc661af4
Andres Rivera <riveraandres1997@gmail.com>
2020-11-29 21:28:58 -0600

    asdas

--- src/conexion.js
+++ src/conexion.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 console.log('Conectando a la base de datos');
 
 var conection = mysql.createConnection({
-    host: '204.2.63.54',
+    host: 'mysql-15388-0.cloudclusters.net',
     port: '15388',
     user: 'andres',
     password: '123andres',
